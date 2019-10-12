5852a7e621ecd1d59f00afc6eb2fbef712a5aba2
Cagatay Yucelen <cagatayyucelen@gmail.com>
2019-10-12 20:53:37 +0300

    default typo fix

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "namespace" {}
 variable "name" {}
 variable "replicacount" {}
 variable "storage_size" {
-  defualt = "10Gi"
+  default = "10Gi"
 }
 variable "storage_class_name" {
   default = "standard"
